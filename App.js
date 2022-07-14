d3a1655b03d745608ab1a40c714ac917aea1c95f
RanushMithila <ranushmithila45@gmail.com>
2022-07-14 15:56:38 +0530

    listing

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import ListingScreen from './app/screens/ListingScreen';
 
 import LoginScreen from './app/screens/LoginScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
 import TestScreen from './app/screens/TestScreen';
 import TextScreen from './app/screens/TextScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 export default function App() {
-  return <MessagesScreen />;
+  return <ListingDetailsScreen />
 }
 
 const styles = StyleSheet.create({
