2d83e38eff93d6504008764eb8376c09aeca6a05
RanushMithila <ranushmithila45@gmail.com>
2022-07-18 12:22:02 +0530

    add image component

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, Image, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 import ListingEditScreen from './app/screens/ListingEditScreen';
+import Screen from './app/components/Screen';
+import ImageInput from './app/components/ImageInput';
+import ImageInputList from './app/components/ImageInputList';
 
 
 export default function App() {
 
-  return <ListingEditScreen />;
+  const [imageUris, setImageUris] = useState([]);
+
+  const handleAdd = (uri) => {
+    setImageUris([...imageUris, uri]);
+  }
+
+  const handleRemove = (uri) => {
+    setImageUris(imageUris.filter(imageUris => imageUris !== uri));
+  }
+
+  return (
+    <Screen>
+      <ImageInputList
+        imageUris={ imageUris }
+        //onAddImage={(uri) => handleAdd(uri)}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+
+      />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
